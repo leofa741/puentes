d3150e2891f5ac1421581bc447fdbb210fa9144c
leonardo arena <leonardofarena@gmail.com>
2023-12-25 21:57:06 -0300

    nn

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -4,6 +4,8 @@ import { Title } from '../components';
 
 
 export const metadata: Metadata = {
+  keywords: 'Puentech, Puentes Digitales, Outsourcing de Innovación, Talento profesional, procesos y tecnologías asociadas, determinan nuestras áreas de conocimiento en Outsourcing de Innovación. ',
+  themeColor: '#000000',
     title: "Home Page | Puentech",
     description: "Puentech es una empresa de desarrollo de software que se especializa en la creación de aplicaciones web y sitios web para pequeñas y medianas empresas.",
      

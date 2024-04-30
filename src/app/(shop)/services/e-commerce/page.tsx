7dd60867b38de40955266dc1abc8aa6760ae9966
leonardo arena <leonardo.arena@engen.com.mx>
2024-04-30 12:17:00 -0300

    nn

--- src/app/(shop)/services/e-commerce/page.tsx
+++ src/app/(shop)/services/e-commerce/page.tsx
@@ -52,7 +52,7 @@ export default function Ecommerce() {
         </div>
 
         <h2 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800">
-          Implentación de E-commerce
+          Implentación de nuestro E-commerce
         </h2>
 
         <p className="text-gray-600 mb-8">
@@ -63,6 +63,94 @@ export default function Ecommerce() {
           administrar sus ventas y productos de manera eficiente.
 
         </p>
+
+        <div className="cards-1  items-center justify-items-center  max-w-[1300px]">
+          <div className=" container">
+            <div className="flex justify-center">
+              <div className="col-lg-12">
+                <h2 className="h2-heading"
+                  style={{
+                    color: "black",
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    fontSize: "2.5rem",
+                    fontFamily: "sans-serif",
+                    textTransform: "uppercase",
+                    letterSpacing: "0.2rem",
+                    textShadow: '4px 4px 9px #000000',
+                  }}>
+                  Implementación
+                </h2>
+
+              </div>
+            </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 w-full  ">
+              <div className=" items-center justify-center mt-8 w-full ">
+
+                <div className="card animacion-block">
+                  <div className="card-image ">
+                    <ImagenCard                    
+                       url={"../assets/img/726_6219f1cbb41bb.webp"}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h4 className="card-title">Cliente</h4>
+                    <p>Podrá registrarse, navegar las diferentes secciones del mismo, agregar  artículos a su carrito de compras,
+                      y una vez finalizada la selección, confirmar su compra eligiendo la forma de pago y tipo de envio.
+                      El Cliente también podrá realizar toda la gestión de compra desde la App.
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+
+              <div className=" flex flex-col items-center justify-center mt-8 w-full">
+
+                <div className="card animacion-block">
+                  <div className="card-image ">
+                    <ImagenCard
+                       url={"../assets/img/ShopApp-para-la-gestión-de-pedidos-y-ventas.jpg"}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h4 className="card-title">Proveedor</h4>
+                    <p>Procesará el pedido realizado por el cliente, actualizará el estado de los pedidos permitiendo al cliente realizar el seguimiento correspondiente.
+                      También podrá gestionar sus artículos, precios, etc.</p>
+                  </div>
+                </div>
+
+              </div>
+
+              <div className=" flex flex-col items-center justify-center mt-8 w-full">
+
+                <div className="card animacion-block">
+                  <div className="card-image ">
+                    <ImagenCard
+                      url={"../assets/img/portada-home.png"}
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h4 className="card-title">Administrador</h4>
+                    <p>Podrá autogestionar toda la plataforma, pudiendo mantener la información de todos los productos adheridos y accediendo a reportes en tiempo real.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* ---- */}
+
+
+
+
+          </div>
+        </div>
+
+
+
+
         <BanerServicesContact />
 
         <h3 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800">

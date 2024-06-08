e7ddf5d40b064de103bb75a59473c81ec2d5425d
leonardo arena <leonardofarena@gmail.com>
2024-06-08 18:02:26 -0300

    nnn

--- src/app/components/bannerfooter/BannerFooter.tsx
+++ src/app/components/bannerfooter/BannerFooter.tsx
@@ -11,7 +11,7 @@ export const BannerFooter = () => {
     <div className="banner-content">
     <h1 className="banner-title">Puentes Digitales</h1>
     <p className="banner-text">Somos una empresa de desarrollo de sistemas web, especializada en la creación de aplicaciones web y móviles.</p>
-   <a href="https://wa.me/+5491136038167?text=Hola%20me%20gustaria%20saber%20mas%20sobre%20los%20concesionarios%20online" className="banner-btn">Contactanos</a>
+   <a href="https://wa.me/+5491141461312?text=Hola%20me%20gustaria%20saber%20mas%20sobre%20los%20concesionarios%20online" className="banner-btn">Contactanos</a>
                
     </div>
     </div>

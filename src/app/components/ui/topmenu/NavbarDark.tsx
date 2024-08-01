ef555a8e917d96009371435230283bc34b883bf9
leonardofarena <leonardofarena@gmail.com>
2024-08-01 00:03:11 +0000

    kk

--- src/app/components/ui/topmenu/NavbarDark.tsx
+++ src/app/components/ui/topmenu/NavbarDark.tsx
@@ -85,7 +85,7 @@ export const NavbarDark = () => {
                 <button className={`${montserrat.className}m-2 pl-3 pr-4 inline-flex justify-center rounded-md transition duration-500     ${pathname == '/soluciones' ? 'bg-gray-900' : ''}`}
                   style={{ letterSpacing: '0.1rem', fontSize: '0.9rem' }}
                   type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
-                  <span>Soluciones </span>
+                  <span>#MachineLearning</span>
                   <svg className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
               </span>

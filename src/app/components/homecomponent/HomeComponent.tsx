349c3be8734d936963a200dcbffee2b10a178622
leonardofarena <leonardofarena@gmail.com>
2024-08-01 15:07:00 +0000

    nn

--- src/app/components/homecomponent/HomeComponent.tsx
+++ src/app/components/homecomponent/HomeComponent.tsx
@@ -17,7 +17,7 @@ export const HomeComponent = () => {
                     <div className="h-100">
                         <h6 className=" section-title text-start  pe-2 font-bold  xs:text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl 2xl:text-3xl">Puentes Digitales</h6>                      
                          
-                        <h1 className="mb-4  font-bold text-4xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl"> #1
+                        <h1 className="mb-4  font-bold text-4xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-5xl 2xl:text-5xl"> Conectando el futuro
                         <span className="text-2xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-4xl xl:text-4xl 2xl:text-4xl "> En Soluciónes digitales </span>
                          <span className="text-1xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-4xl xl:text-4xl 2xl:text-4xl "> con </span>   
                          <span className="text-2xl xs:text-5xl sm:text-5xl md:text-5xl lg:text-4xl xl:text-4xl 2xl:text-4xl ">     100% de satisfacción</span>                   

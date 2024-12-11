b3aabe1f366b7677c9eb9bd8219d5584ccaef963
leonardo <leonardofarena@gmail.com>
2024-12-10 22:19:24 -0300

    x

--- src/app/(shop)/services/e-commerce/page.tsx
+++ src/app/(shop)/services/e-commerce/page.tsx
@@ -99,7 +99,7 @@ export default function Ecommerce() {
               description="Camiseta unisex "
               sizes={['S', 'M', 'L', 'XL']}
               scala={[0.3, 0.3, 0.3]}
-              stile={{ width: '360px', height: '500px' }}   
+              stile={{ width: '300px', height: '500px' }}   
             />
           </div>
         </div>

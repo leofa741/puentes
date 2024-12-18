68efe383d7dc7ddc4c8056898ff619f2bb5733b5
leonardo <leonardofarena@gmail.com>
2024-12-18 19:10:15 -0300

    dd

--- src/app/components/carrousel/scroll.tsx
+++ src/app/components/carrousel/scroll.tsx
@@ -2,5 +2,10 @@
   <div className="mouse">
     <div className="mouse-wheel"></div>
   </div>
-  {/* <p className="scroll-text">Desliza hacia abajo</p> */}
+  <div className="arrows">
+    <div className="arrow"></div>
+    <div className="arrow"></div>
+
+</div>
+
 </div>
